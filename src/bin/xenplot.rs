40f402f1c9d50f123d34a0100def7b24abfe1a92
Christopher Ariza <ariza@flexatone.com>
2024-03-09 18:39:50 +0100

    progress on plotting

--- src/bin/xenplot.rs
+++ src/bin/xenplot.rs
@@ -3,13 +3,16 @@ use xensieve::Sieve;
 
 fn plot_sieves(
         sieve_strings: Vec<String>,
-        range: (i32, i32),
+        range: (i32, i32), // value range used to plot sieve
         ) -> Result<(), Box<dyn std::error::Error>> {
 
-    let fig_w: u32 = 700;
-    let fig_h: u32 = 480;
+    let fig_w: u32 = 600;
+    let fig_h: u32 = 700;
     let fig_y_label_size = 50;
+    let fig_x_label_size = 140; // bottom space for vertical labels
     let count_col: i32 = sieve_strings.len().try_into()?;
+    let line_color = &RGBColor(30, 30, 180).mix(0.6);
+    let line_thickness = 5; // Adjust the thickness of the line
 
     let root = SVGBackend::new("test-plot.svg", (fig_w, fig_h)).into_drawing_area();
     root.fill(&WHITE)?;
@@ -17,7 +20,7 @@ fn plot_sieves(
     // .caption("Sieve", ("sans-serif", 30))
     let mut chart = ChartBuilder::on(&root)
         .margin(10)
-        .x_label_area_size(30)
+        .x_label_area_size(fig_x_label_size)
         .y_label_area_size(fig_y_label_size)
         .build_cartesian_2d(0..count_col, range.0-1..range.1+1)?;
 
@@ -26,14 +29,12 @@ fn plot_sieves(
         .x_label_style(("sans-serif", 20).into_font().color(&RGBAColor(0, 0, 0, 0.0)))
         .draw()?;
 
-    let line_color = &RGBColor(30, 120, 120).mix(0.5);
-    let line_thickness = 12; // Adjust the thickness of the line
     let col_width = (fig_w as i32 - fig_y_label_size) / count_col;
 
     // for (x, lines, label) in column_data {
     for (x, label) in sieve_strings.iter().enumerate() {
         let lines: Vec<i128> = Sieve::new(&label).iter_value(
-                value_min as i128..value_max as i128).collect();
+                range.0 as i128..range.1 as i128).collect();
 
         for &y in &lines {
             // Draw a horizontal line at each specified y-value
@@ -45,16 +46,15 @@ fn plot_sieves(
 
         // do not try to center label; instead, just shift from left bound by a margin
         let label_position = (
-                fig_y_label_size + (col_width * x as i32) + 5,
-                fig_h as i32 - 20,
+                (fig_y_label_size + 20) + ((col_width - 4) * x as i32),
+                fig_h as i32 - fig_x_label_size,
                 ); // Example calculation for label position
 
         root.draw_text(
             label,
-            &("sans-serif", 16).into_font().color(&BLACK),
+            &("sans-serif", 20).into_font().color(&BLACK).transform(FontTransform::Rotate90),
             label_position,
         )?;
-
     }
     Ok(())
 }
